acd36b487ae9e66333a7c0ef8882d2dc1429e690
Mickael <mickaelvinet79@gmail.com>
2019-04-04 11:44:47 +0200

    Changement nom dossier

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {withStyles} from "@material-ui/core";
 import Login from "./composant/User/Login/Login.js";
 import CreateUser from './composant/User/CreateUser/CreateUser.js'
 import Acceuil from "./composant/Acceuil/Acceuil.js";
-import ValidUser from "./composant/Admin/ValidUser/ValidateUser.js";
+import ValidUser from "./composant/Admin/ValidateUser/ValidateUser.js";
 import Message from "./composant/Message/Message.js";
 import VehicleList from "./composant/Admin/VehicleList/VehicleList";
 
