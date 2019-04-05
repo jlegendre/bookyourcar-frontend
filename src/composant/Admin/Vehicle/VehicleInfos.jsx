0072c2a11a002cb6488ae625d1baf439549aa728
HALLAY Romain <romain.hallay2016@campus-eni.fr>
2019-04-05 15:55:06 +0200

    Debut vehicule info 4

--- src/composant/Admin/Vehicle/VehicleInfos.jsx
+++ src/composant/Admin/Vehicle/VehicleInfos.jsx
@@ -8,18 +8,48 @@ import {
     Icon,
     colors
 } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/es/TextField/TextField";
+import Grid from "@material-ui/core/Grid";
 
 const VehicleInfos = props => {
 
     const {classes, fetchVehicleInfos, datapage, match} = props;
 
     useEffect(() => {
-        fetchVehicleInfos(match.param.vehId);
+        console.log(match)
+        fetchVehicleInfos(match.params.vehId);
     }, []);
 
     return (
         <div>
             <Paper className={classes.paper}>
+                <Grid direction={"column"}>
+                    <Grid direction={"row"}>
+                        <p>PHOTO</p>
+                        <Grid direction={"column"}>
+                            <Grid direction={"row"}>
+                                <TextField label={datapage.vehBrand}/>
+                                <TextField label={datapage.vehModel}/>
+                            </Grid>
+                            <TextField label={datapage.vehRegistration}/>
+                            <TextField label={datapage.vehColor}/>
+                        </Grid>
+                    </Grid>
+                    <TextField label={datapage.vehNumberplace}/>
+                    <TextField label={datapage.vehTypeEssence}/>
+                    <TextField label={datapage.poleName}/>
+
+                </Grid>
+                <Grid direction={"row"}>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Mise à jour
+                    </Button>
+                    <Button variant="contained" color="secondary" className={classes.button}>
+                        Supprimer
+                    </Button>
+                </Grid>
 
             </Paper>
         </div>
