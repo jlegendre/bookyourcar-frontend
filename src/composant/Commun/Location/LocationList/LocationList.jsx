d0c77c9b083e69e2c1d4c95bc7f7144cc0541766
Mickael <mickaelvinet79@gmail.com>
2019-09-18 21:39:04 +0200

    Datatable pour les reservations

--- src/composant/Commun/Location/LocationList/LocationList.jsx
+++ src/composant/Commun/Location/LocationList/LocationList.jsx
@@ -1,42 +1,76 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import * as PropTypes from 'prop-types';
-import LocationItem from "./LocationItem";
 import PopupValidateReservation from "./PopupValidateReservation/PopupValidateReservation";
+import DataTable from 'react-data-table-component';
+import Paper from "@material-ui/core/Paper";
+import {Typography} from "@material-ui/core";
 
 const LocationList = props => {
 
-    const {locations, completeView, fetchDetailLocation} = props;
+    const {locations, fetchDetailLocation} = props;
 
     const [popupOpen, setPopupOpen] = useState(false);
     const [currentLocation, setCurrentLocation] = useState(null);
-    const [dataCurrentLocation, setDataCurrentLocation] = useState(null);
 
-    useEffect(() => {
-
-        if(completeView && currentLocation != null && popupOpen) {
-            fetchDetailLocation(currentLocation, success => {
-                setPopupOpen(true);
-                setDataCurrentLocation(success);
-            })
+    const colonnes = [
+        {
+            name: <Typography>Utilisateur</Typography>,
+            selector: 'userFriendlyName',
+            sortable: true,
+            cell: row => <Typography>{row['userFriendlyName']}</Typography>
+        },
+        {
+            name: <Typography>Date début</Typography>,
+            selector: 'dateDebutResa',
+            sortable: true,
+            cell: row => <Typography>{row['dateDebutResa']}</Typography>
+        },
+        {
+            name: <Typography>Date fin</Typography>,
+            selector: 'dateFinResa',
+            sortable: true,
+            cell: row => <Typography>{row['dateFinResa']}</Typography>
+        },
+        {
+            name: <Typography>Véhicule</Typography>,
+            selector: 'vehicleFriendlyName',
+            sortable: true,
+            cell: row => <Typography>{row['vehicleFriendlyName']}</Typography>
+        },
+        {
+            name: <Typography>Etat</Typography>,
+            selector: 'locationState',
+            sortable: true,
+            cell: row => <Typography>{row['locationState']}</Typography>
         }
+    ];
 
-    }, [popupOpen, fetchDetailLocation, setDataCurrentLocation, completeView, currentLocation]);
+    const openLocation = row => {
+        console.log(row);
+        fetchDetailLocation(row.locationId, success => {
+            setCurrentLocation(success);
+            setPopupOpen(true)
+            console.log(success);
+        });
+    };
 
     return (
         <React.Fragment>
-            {locations && locations.map(item =>
-                <LocationItem
-                    onClick={() => {setPopupOpen(true); setCurrentLocation(item.locationId)}}
-                    key={item.locationId}
-                    data={item}
-                    completeView={completeView}
+
+            <Paper>
+                <DataTable
+                    title={<Typography component={"h4"} variant={"h4"}>Location</Typography>}
+                    columns={colonnes}
+                    data={locations}
+                    onRowClicked={openLocation}
+                    highlightOnHover
                 />
-            )}
+            </Paper>
 
             <PopupValidateReservation
                 open={popupOpen}
                 onClose={() => setPopupOpen(false)}
-                data={dataCurrentLocation}
+                data={currentLocation}
             />
         </React.Fragment>
     )
