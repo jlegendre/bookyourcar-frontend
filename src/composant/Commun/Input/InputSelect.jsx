61a331ac51da84fda43de0dea26732394a8f85a8
Mickael <mickaelvinet79@gmail.com>
2019-09-30 08:38:48 +0200

    amélioration InputSelect

--- src/composant/Commun/Input/InputSelect.jsx
+++ src/composant/Commun/Input/InputSelect.jsx
@@ -7,59 +7,35 @@ import FormHelperText from "@material-ui/core/FormHelperText/index";
 
 const InputSelect = props => {
 
-        const {fullWidth, onChange, data, name, label, message, value, id, className, disabled} = props;
+    const {fullWidth, onChange, data, name, label, message, value, id, className, disabled} = props;
 
 
-        if (disabled) {
-            return (<FormControl margin={"normal"} fullWidth={fullWidth} disabled>
-                <InputLabel>{label}</InputLabel>
-                <Select
-                    onChange={onChange}
-                    value={value}
-                    error={message && !!message[name]}
-                    isDisabled={disabled}
-                    inputProps={{
-                        name: name,
-                        id: id
-                    }}
-                    className={className}
-                >
-                    {data && data.map((item, i) =>
-                        <MenuItem key={i} value={item.value}>{item.label}</MenuItem>
-                    )}
-                </Select>
-                {message && message[name] &&
-                <FormHelperText>{message[name][0]}</FormHelperText>
-                }
+    return (
+        <FormControl margin={"normal"} fullWidth={fullWidth} disabled={disabled}>
+            <InputLabel>{label}</InputLabel>
+            <Select
+                onChange={onChange}
+                value={value}
+                error={message && !!message[name]}
+                isDisabled={disabled}
+                inputProps={{
+                    name: name,
+                    id: id
+                }}
+                className={className}
+            >
+                {data && data.map((item, i) =>
+                    <MenuItem key={i} value={item.value}>{item.label}</MenuItem>
+                )}
+            </Select>
+            {message && message[name] &&
+            <FormHelperText>{message[name][0]}</FormHelperText>
+            }
 
-            </FormControl>)
-        } else {
-            return (
-                <FormControl margin={"normal"} fullWidth={fullWidth}>
-                    <InputLabel>{label}</InputLabel>
-                    <Select
-                        onChange={onChange}
-                        value={value}
-                        error={message && !!message[name]}
-                        isDisabled={disabled}
-                        inputProps={{
-                            name: name,
-                            id: id
-                        }}
-                        className={className}
-                    >
-                        {data && data.map((item, i) =>
-                            <MenuItem key={i} value={item.value}>{item.label}</MenuItem>
-                        )}
-                    </Select>
-                    {message && message[name] &&
-                    <FormHelperText>{message[name][0]}</FormHelperText>
-                    }
+        </FormControl>
+    )
+};
 
-                </FormControl>)
-        }
-    }
-;
 
 InputSelect.defaultProps = {
     fullWidth: true
